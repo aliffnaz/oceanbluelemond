3d7ac815ac8ef9c604762f1fbbc687c66e9950e0
am <150236487+syedikramstudent@users.noreply.github.com>
2024-01-09 14:28:41 +0800

    Create staff.java

---
+++ src/main/java/com/heroku/java/model/staff.java
@@ -0,0 +1,83 @@
+package com.heroku.java.model;
+
+public class staff {
+
+    private String staffICNumber;
+    private String staffName;
+    private String staffGender;
+    private String staffPhoneNumber;
+    private String staffRace;
+    private String staffReligion;
+    private String staffMaritalStatus;
+    private String staffAddress;
+    private String staffRole;
+    private String staffStatus;
+    private String managerICNumber;
+
+    public String getStaffICNumber() {
+		return staffICNumber;
+	}
+	public void setStaffICNumber(String staffICNumber) {
+		this.staffICNumber = staffICNumber;
+	}
+	public String getStaffName() {
+		return staffName;
+	}
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+	public String getStaffGender() {
+		return staffGender;
+	}
+	public void setStaffGender(String staffGender) {
+		this.staffGender = staffGender;
+	}
+	public String getStaffPhoneNumber() {
+		return staffPhoneNumber;
+	}
+	public void setStaffPhoneNumber(String staffPhoneNumber) {
+		this.staffPhoneNumber = staffPhoneNumber;
+	}
+	public String getStaffRace() {
+		return staffRace;
+	}
+	public void setStaffRace(String staffRace) {
+		this.staffRace = staffRace;
+	}
+	public String getStaffReligion() {
+		return staffReligion;
+	}
+	public void setStaffReligion(String staffReligion) {
+		this.staffReligion = staffReligion;
+	}
+	public String getStaffMaritalStatus() {
+		return staffMaritalStatus;
+	}
+	public void setStaffMaritalStatus(String staffMaritalStatus) {
+		this.staffMaritalStatus = staffMaritalStatus;
+	}
+	public String getStaffAddress() {
+		return staffAddress;
+	}
+	public void setStaffAddress(String staffAddress) {
+		this.staffAddress = staffAddress;
+	}
+	public String getStaffRole() {
+		return staffRole;
+	}
+	public void setStaffRole(String staffRole) {
+		this.staffRole = staffRole;
+	}
+	public String getStaffStatus() {
+		return staffStatus;
+	}
+	public void setStaffStatus(String staffStatus) {
+		this.staffStatus = staffStatus;
+	}
+	public String getManagerICNumber() {
+		return managerICNumber;
+	}
+	public void setManagerICNumber(String managerICNumber) {
+		this.managerICNumber = managerICNumber;
+	}
+} 
