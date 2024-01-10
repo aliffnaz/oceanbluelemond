8df5b32706b1dd861539cadd85c5eab17fb47ff8
am <150236487+syedikramstudent@users.noreply.github.com>
2024-01-10 22:40:47 +0800

    Create service.java

---
+++ src/main/java/com/heroku/java/model/service.java
@@ -0,0 +1,42 @@
+package com.heroku.java.model;
+
+public class service {
+
+    private String serviceID;
+    private String serviceName;
+    private String serviceType;
+    private String servicePrice;
+    private String serviceStatus;
+
+    public String getServiceID() {
+		return serviceID;
+	}
+	public void setServiceID(String serviceID) {
+		this.serviceID = serviceID;
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	public String getServiceType() {
+		return serviceType;
+	}
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+	public String getServicePrice() {
+		return servicePrice;
+	}
+	public void setServicePrice(String servicePrice) {
+		this.servicePrice = servicePrice;
+	}
+	public String getServiceStatus() {
+		return serviceStatus;
+	}
+	public void setServiceStatus(String serviceStatus) {
+		this.serviceStatus = serviceStatus;
+	}
+
+}
