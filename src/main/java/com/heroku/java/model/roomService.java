4b1e639890b149ad1243c5223b8c75f6ebccbb0e
am <150236487+syedikramstudent@users.noreply.github.com>
2024-01-21 15:10:10 +0800

    Update roomService.java

--- src/main/java/com/heroku/java/model/roomService.java
+++ src/main/java/com/heroku/java/model/roomService.java
@@ -2,21 +2,21 @@ package com.heroku.java.model;
 
 public class roomService extends service {
 
-    private String balance;
+    private int balance;
 
     public roomService(){
 
     }
 
-    public roomService(String serviceID, String serviceName, String serviceType, String servicePrice, String serviceStatus, String balance){
+    public roomService(int serviceID, String serviceName, String serviceType, double servicePrice, String serviceStatus, int balance){
         super(serviceID, serviceName, serviceType, servicePrice, serviceStatus);
         this.balance = balance;
     }
 
-    public String getBalance() {
+    public int getBalance() {
 		return balance;
 	}
-	public void setBalance(String balance) {
+	public void setBalance(int balance) {
 		this.balance = balance;
 	}
 
