70799ec850956b6791b4d753f6ef2a32ce94961e
am <150236487+syedikramstudent@users.noreply.github.com>
2024-05-14 22:55:09 +0800

    Update ReservationController.java

--- src/main/java/com/heroku/java/controller/ReservationController.java
+++ src/main/java/com/heroku/java/controller/ReservationController.java
@@ -1140,9 +1140,6 @@ public class ReservationController {
 
                 reservations.add(reservation);
                 model.addAttribute("reservations", reservations);
-                if (reservations.isEmpty()) {
-                    model.addAttribute("messageNoResult", "No results found");
-                }
 
             }
             connection.close();
@@ -1216,9 +1213,6 @@ public class ReservationController {
 
                 reservations.add(reservation);
                 model.addAttribute("reservations", reservations);
-                if (reservations.isEmpty()) {
-                    model.addAttribute("messageNoResult", "No results found for \"" + searchInput + "\"");
-                }
             }
             connection.close();
 
